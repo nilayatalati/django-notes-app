8cc40822c0c039a44d00a267cc821974a3db67cd
nilayatalati <nilaytalati@yahoo.com>
2026-07-03 17:55:52 +0530

    Update ALLOWED_HOSTS configuration in settings.py

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&dzi#zsb(hz6p(s#anunt&#-a%ohr2hld71*i72*^exvw-yq$y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = 'localhost', '127.0.0.1', '*']
 
 
 # Application definition
